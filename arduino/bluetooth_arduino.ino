bc4f3f57c5c7b51481c1640941e68de8fe3883da
LeeDong <joup23@naver.com>
2020-05-01 17:40:54 +0900

    update

--- arduino/bluetooth_arduino.ino
+++ arduino/bluetooth_arduino.ino
@@ -2,8 +2,8 @@
 // 업로드시 RX, TX핀을 제거하고 업로드
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);   
-  pinMode(2, OUTPUT);             
+  pinMode(LED_BUILTIN, OUTPUT); //led1번 지정
+  pinMode(2, OUTPUT);           //led2번 지정
 
 //Begin Serial Monitor
   Serial.begin(9600); //블루투스 시리얼
@@ -20,12 +20,12 @@ void loop() {
   static boolean flag = true;
 
 
-  if (Serial.available()) {    
-    char br = Serial.read();
+  if (Serial.available()) {     //수신된 데이터가 있다면
+    char br = Serial.read();  //수신된 데이터를 읽어와 br이라는 변수에 저장
 
 
   switch(br){
-      case '1' :
+      case '1' :  //br이 1이면 3초동안 0.5초 간격으로 led 2번이 깜빡임
         for(int i=0;i<3;i++){
         digitalWrite(2, HIGH);
         digitalWrite(LED_BUILTIN, LOW);
@@ -36,7 +36,7 @@ void loop() {
       }
       break;
 
-      case '2' :     
+      case '2' :     //br이 2이면 led 1번이 3초동안 깜빡인 다음 led 2번이 3초동안 깜빡임 
         for(int i=0;i<3;i++){
         digitalWrite(2, HIGH);
         digitalWrite(LED_BUILTIN, LOW);
@@ -47,7 +47,7 @@ void loop() {
       }
       break;
 
-      case '3' :    
+      case '3' :    //br이 3이면 led2번에 불이 들어옴
         digitalWrite(2, HIGH);
         digitalWrite(LED_BUILTIN, LOW);
         delay(30);
